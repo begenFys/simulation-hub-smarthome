ca0d297628d842ac409cacbd48016743626592b9
begenFys <vip.nikita.2004@list.ru>
2023-07-14 00:52:32 +0500

    Nice, WHOISHERE is already here! Decode and encode completed. Next step: request processing, different device classes, TESTS

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # TODO - что нужно будет сделать
 # в общем планированию реализовать класс hub, который будет отсылать запросы и хранить список сенсоров, устройств
 # посылать запросы и принимать
-# 1. Кодирование декодирование packet из base64
+# 1. Кодирование декодирование packet из base64 +
 # 2. Подключение к серверу
-# 3. Изучить типы завершения программы, как их менять
+# 3. Изучить типы завершения программы, как их менять +
 # 4. Прописать случаи завершения программы
-# 5. uleb128 посмотреть что это
-# 6. crc кодирование конечной суммы payload
+# 5. uleb128 посмотреть что это +
+# 6. crc кодирование конечной суммы payload +
 # 7. работа с разными типами устройств
-# 8. проверка на игнор пакета(гарантируется ли?)
+# 8. проверка на игнор пакета(гарантируется ли?) +
 
 # TODO - какие функции пригодятся
-# 1. Функция из 16 вида в ULEB128 и наоборот
+# 1. Функция из 16 вида в ULEB128 и наоборот +
 # 2.
 
 # ВСПОМНИТЬ
@@ -24,56 +24,80 @@ import requests
 import base64
 import leb128
 
+BROADCAST_DST: int = 0x3fff
+# Не совсем логичный следующее действие, так как заглавные буквы используют для обозначения констант,
+# а дальше мы будем PACKETS изменять, но это сделано чтобы было видно и понятно, так что не думаю что это критично.
+# Главное я пояснил момент
+PACKETS: List[bytearray] = []  # пакеты, которые мы будем потом оформлять в запрос
 
+
+class CMD:  # чтобы в циферках не запутаться)
+    WHOISHERE = 1
+    IAMHERE = 2
+    GETSTATUS = 3
+    STATUS = 4
+    SETSTATUS = 5
+
+
+# работа с форматов uleb128
 def uleb128_decode(data: bytearray) -> int:
     return leb128.u.decode(data)
 
+
 def uleb128_encode(data: int) -> bytearray:
-    return leb128.u.encode(24938124)
+    return leb128.u.encode(data)
+
+
 def uleb128_length(data: bytearray) -> int:
     length = 0
     index = 0
-
     while True:
         byte = data[index]
         length += 1
         index += 1
-
         if byte & 0x80 == 0:
             break
 
     return length
 
-class Hub:
-    def __init__(self, url: str, hub_src: int):
-        self.__url = url
-        self.__hub_src = hub_src
-        self.__sensors_src = []
-        self.__devices_src = []
+
+# считаем crc8
+def calculate_crc8(data: bytearray):
+    crc = 0x00  # Начальное значение CRC
+    for byte in data:
+        crc ^= byte  # Исключающее ИЛИ между текущим значением CRC и байтом данных
+        for _ in range(8):
+            if crc & 0x80:  # Проверка старшего бита CRC
+                crc = (crc << 1) ^ 0x1d  # Сдвиг влево и XOR с полиномом 0x1d
+            else:
+                crc <<= 1  # Сдвиг влево
+        crc &= 0xff  # Ограничение CRC до 8 бит
+
+    return crc
+
+
+# преобразуем строк в нужный нам вид
+def str_to_bytearray(data: str) -> bytearray:
+    byte_array = bytearray()
+    byte_array.append(len(data))
+    byte_array.extend(data.encode())
+    return byte_array
 
 
 class UrlCoder:
-
-    def parse_payload(self, payload: bytearray) -> Dict:
+    @staticmethod
+    def parse_payload(payload: bytearray) -> Dict:
         payload_json = {"src": 0, "dst": 0, "serial": 0, "dev_type": 0, "cmd": 0}
         ind = 0
         # src
-        src_length = uleb128_length(payload)
-        if src_length == 2:
-            payload_json["src"] = uleb128_decode(payload[ind:ind+2])
-            ind += 2
-        elif src_length == 1:
-            payload_json["src"] = uleb128_decode(payload[ind])
-            ind += 1
+        src_length = uleb128_length(payload[ind:])
+        payload_json["src"] = uleb128_decode(payload[ind:ind + src_length]) if src_length > 1 else payload[ind]
+        ind += src_length
 
-        #dst
-        dst_length = uleb128_length(payload)
-        if dst_length == 2:
-            payload_json["dst"] = uleb128_decode(payload[ind:ind + 2])
-            ind += 2
-        elif dst_length == 1:
-            payload_json["dst"] = uleb128_decode(bytearray(payload[ind]))
-            ind += 1
+        # dst
+        dst_length = uleb128_length(payload[ind:])
+        payload_json["dst"] = uleb128_decode(payload[ind:ind + dst_length]) if dst_length > 1 else payload[ind]
+        ind += dst_length
 
         payload_json["serial"] = payload[ind]
         ind += 1
@@ -82,9 +106,10 @@ class UrlCoder:
         payload_json["cmd"] = payload[ind]
         return payload_json
 
-    def decode(self, response_base64: bytes) -> Union[List[Dict], None]:
+    @staticmethod
+    def decode(response_base64: bytes) -> Union[List[Dict], None]:
         # вспомогательные функции
-        def base64_to_bytearray(inp: bytes):
+        def base64_to_bytearray(inp: bytes) -> Union[bytearray, None]:
             try:
                 str = base64.urlsafe_b64decode(inp + b'=' * (len(inp) % 4))
                 hex_str = bytearray(str).hex()
@@ -93,30 +118,15 @@ class UrlCoder:
             except:
                 return None
 
-        def split_packets(response_bytes: bytearray):
-            def calculate_crc8(data: bytearray):
-                crc = 0x00  # Начальное значение CRC
-
-                for byte in data:
-                    crc ^= byte  # Исключающее ИЛИ между текущим значением CRC и байтом данных
-                    for _ in range(8):
-                        if crc & 0x80:  # Проверка старшего бита CRC
-                            crc = (crc << 1) ^ 0x1d  # Сдвиг влево и XOR с полиномом 0x1d
-                        else:
-                            crc <<= 1  # Сдвиг влево
-                    crc &= 0xff  # Ограничение CRC до 8 бит
-
-                return crc
-
+        def split_packets(response_bytes: bytearray) -> List[Dict]:
             packets = []
             ind = 0
             while ind < len(response_bytes):
-                packet = {"length": response_bytes[ind], "payload": bytearray, "crc8": 0}
+                packet = {"length": response_bytes[ind], "payload": bytearray(), "crc8": 0}
                 ind += 1
                 packet["payload"] = response_bytes[ind:ind + packet["length"]]
                 ind += packet["length"]
                 packet["crc8"] = response_bytes[ind]
-
                 if packet["crc8"] == calculate_crc8(packet["payload"]):  # игнор пакет, если контрольная не совпадает
                     packets.append(packet)
                 ind += 1
@@ -127,31 +137,81 @@ class UrlCoder:
         if response_bytes:
             packets = split_packets(response_bytes)
             for ind in range(len(packets)):
-                print(packets[ind]["payload"])
-                packets[ind]["payload"] = self.parse_payload(packets[ind]["payload"])
+                packets[ind]["payload"] = UrlCoder.parse_payload(packets[ind]["payload"])
             return packets
         else:
             return None
 
-    def encode(self, responses: List[dict]) -> bytes: # bytes ли?
-        pass
+    @staticmethod
+    def encode_payload(src: int, dst: int, serial: int, dev_type: int, cmd: int) -> bytearray:
+        payload = bytearray()
+        payload.extend(uleb128_encode(src))
+        payload.extend(uleb128_encode(dst))
+        payload.extend(uleb128_encode(serial))
+        payload.append(dev_type)
+        payload.append(cmd)
+
+        return payload
+
+    @staticmethod
+    def encode_packet(payload: bytearray) -> bytearray:
+        packet = bytearray()
+        packet.append(len(payload))
+        packet.extend(payload)
+        packet.append(calculate_crc8(payload))
+        return packet
+
+    @staticmethod
+    def encode(packets: List[bytearray]) -> bytes:
+        response = bytearray()
+        for packet in packets:
+            response.extend(packet)
+        return base64.urlsafe_b64encode(response)
+
+
+class Hub:
+    def __init__(self, src: int, dev_name: str) -> None:
+        self.src = src
+        self.__serial = 1
+        self.dev_type = 1
+        self.dev_name = dev_name
+
+    @property
+    def serial(self) -> int:
+        self.__serial += 1
+        return self.__serial - 1
+
+    def WHOISHERE(self) -> bytearray:
+        payload = UrlCoder.encode_payload(self.src, BROADCAST_DST, self.__serial, self.dev_type, CMD.WHOISHERE)
+
+        cmd_body = bytearray()
+        cmd_body.extend(str_to_bytearray(self.dev_name))
+        payload.extend(cmd_body)
+
+        packet = UrlCoder.encode_packet(payload)
+        return packet
 
 
 if __name__ == "__main__":
     URL: str = sys.argv[1]
     if "http" not in URL:
         URL = f"http://{URL}"
-    HUB_SRC: str = sys.argv[2]  # в uleb
-    urlcoder = UrlCoder()
+    HUB_SRC: int = int(sys.argv[2], 16)  # в uleb
 
-    res = requests.post(URL, timeout=300)
-    if res.status_code == 200:
-        print(res.content)
-        if res.content:
-            response_decode = urlcoder.decode(res.content)
-            print(response_decode)
-    elif res.status_code == 204:
-        sys.exit(0)
-    else:
-        sys.exit(99)
+    hub = Hub(HUB_SRC, "HUB01")
+    PACKETS.append(hub.WHOISHERE())
+    while PACKETS:
+        response = UrlCoder.encode(PACKETS)
+        res = requests.post(URL, data=response, timeout=300)
+        PACKETS.clear()
+        if res.status_code == 200:
+            print(res.content)
+            print(UrlCoder.decode(res.content))
+        elif res.status_code == 204:
+            sys.exit(0)
+        else:
+            sys.exit(99)
+
+
+
 # python main.py localhost:9998 ef0
--- test.py
+++ test.py
@@ -1,48 +1,5 @@
-import crc8
 import base64
 
-
-def base64_to_bytearray(inp: bytes, view: bool = False):
-    try:
-        string = base64.urlsafe_b64decode(inp + b'=' * (len(inp) % 4))
-        hex_str = bytearray(string).hex()
-        byte_array = bytearray([int(hex_str[i:i + 2], 16) for i in range(0, len(hex_str), 2)])
-        if view:
-            hex_view = [hex_str[i:i + 2] for i in range(0, len(hex_str), 2)]
-            print("hex_view:", hex_view)
-            bin_view = " ".join([bin(int(byte, 16))[2:] for byte in hex_view])
-            print("bin_view", bin_view)
-        return byte_array
-    except:
-        return None
-
-
-response_bytes = base64_to_bytearray(b"DAH_fwEBAQVIVUIwMeE", True)
-print(response_bytes)
-
-ind = 0
-
-
-def calculate_crc8(data: bytearray):
-    crc = 0x00  # Начальное значение CRC
-
-    for byte in data:
-        crc ^= byte  # Исключающее ИЛИ между текущим значением CRC и байтом данных
-        for _ in range(8):
-            if crc & 0x80:  # Проверка старшего бита CRC
-                crc = (crc << 1) ^ 0x1d  # Сдвиг влево и XOR с полиномом 0x1d
-            else:
-                crc <<= 1  # Сдвиг влево
-        crc &= 0xff  # Ограничение CRC до 8 бит
-
-    return crc
-
-
-while ind < len(response_bytes):
-    packet_length = response_bytes[ind]
-    packet = response_bytes[ind + 1:ind + packet_length + 1]
-    ind += packet_length + 1
-    sm_crc8 = response_bytes[ind]
-    ind += 1
-
-    print(packet_length, packet, sm_crc8, calculate_crc8(packet))
+# DbMG_38BBgaI0Kv6kzGK
+resp = bytearray([0x0d, 0xb3, 0x06, 0xff, 0x7f, 0x01, 0x06, 0x06,  0x88, 0xd0, 0xab, 0xfa, 0x93, 0x31, 0x8a])
+print(base64.urlsafe_b64encode(resp))
