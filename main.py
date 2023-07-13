24e69b8a9b8c34afd7993c2e58077e756a0758b9
begenFys <vip.nikita.2004@list.ru>
2023-07-13 17:41:40 +0500

    My tries...

--- main.py
+++ main.py
@@ -18,7 +18,12 @@
 # 1. Битовые маски
 # 2. Догнать бинарное число до нужного кол-ва бит
 import sys
+import requests
+import urllib.parse
+import base64
 
+
+#
 
 class Hub:
     def __init__(self, url: str, hub_src: int):
@@ -28,6 +33,34 @@ class Hub:
         self.__devices_src = []
 
 
+class UrlCoder:
+    @staticmethod
+    def encode(text: str):
+        pass
+
+    @staticmethod
+    def decode(text: bytes):
+        decoded_data = urllib.parse.unquote(text)
+        print(decoded_data)
+        decoded_data = base64.urlsafe_b64decode(decoded_data + b'=' * (-len(decoded_data) % 4))
+        print(decoded_data)
+
+
 if __name__ == "__main__":
     URL: str = sys.argv[1]
-    HUB_SRC: int = int(sys.argv[2])  # тут улеб херня
+    if "http" not in URL:
+        URL = f"http://{URL}"
+    HUB_SRC: str = sys.argv[2]  # в uleb
+
+    print(URL)
+    res = requests.post(URL)
+    while res.status_code != 204:
+        print(res.content)
+        res = requests.post(URL)
+    # if res.status_code == 200:
+    #
+    # elif res.status_code == 204:
+    #     sys.exit(0)
+    # else:
+    #     sys.exit(99)
+# python main.py localhost:9998 ef0
--- test.py
+++ test.py
@@ -1,5 +1,42 @@
+import urllib.parse
+import base64
 import json
-obj = {"name" : "John"}
-obj_json = json.dumps(obj)
-print(obj_json)
-print(obj_json.encode())
+
+def base64_url_decode(inp):
+    return base64.urlsafe_b64decode(inp + b'=' * (4 - len(inp) % 4))
+
+#print(base64_url_decode(b"DbMG_38SBgbMi6f3lDF4"))
+
+h = b'\r\xb3\x06\xff\x7f\x14\x06\x06\x94\x8d\xa7\xf7\x941\x14'
+
+packets = [
+    {
+        "length": 13,
+        "payload": {
+            "src": 819,
+            "dst": 16383,
+            "serial": 1,
+            "dev_type": 6,
+            "cmd": 6,
+            "cmd_body": {
+                "timestamp": 1688984021000
+            }
+        },
+        "crc8": 138
+    }
+]
+
+# json -> str
+json_str = "".join(json.dumps(packets).split()) # игнор пробельных знаков
+
+# str -> bytes
+encode_str = "".join(json.dumps(packets).split()).encode("utf-8")
+
+# bytes -> base64url
+base64url = base64.urlsafe_b64encode(encode_str)
+
+print(json_str)
+print(base64url)
+print(base64.urlsafe_b64decode(base64url))
+print(base64.urlsafe_b64decode(base64url).decode())
+print(json.loads(base64.urlsafe_b64decode(base64url).decode()))
